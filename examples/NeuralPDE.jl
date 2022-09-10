87504d2604cba8d2e84db1560a819600658d23e9
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2022-09-10 13:08:40 +0200

    first version of a mesh based on a PDE solution; pipeline works but the math behind it is buggy

--- examples/NeuralPDE.jl
+++ examples/NeuralPDE.jl
@@ -4,6 +4,8 @@ using ModelingToolkit
 using IntervalSets
 using Makie
 using GLMakie
+using MeshIO  # good package
+using FileIO  # good package
 
 GLMakie.activate!()
 
@@ -114,13 +116,79 @@ u_predict = reshape([first(phi([x,y],res.u)) for x in xs for y in ys],(length(xs
 # PLOTTING DONE BY MAKIE.jl
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# 2D Code
-#f = Figure()
-#Axis(f[1, 1])
-#co = contourf!(xs, ys, u_predict)
-#Colorbar(f[1, 2], co)
+# # 2D Code
+# f = Figure()
+# Axis(f[1, 1])
+# co = contourf!(xs, ys, u_predict)
+# Colorbar(f[1, 2], co)
 
-scene = Makie.Scene(resolution = (400,400));
-Makie.surface(xs, ys, u_predict, axis=(type=Axis3,))
+# # 3D Code
+# scene = Makie.Scene(resolution = (400,400));
+# surface = Makie.surface(xs, ys, u_predict, axis=(type=Axis3,))
 
-# TODO: export the plot to a mesh and then to a '.stl' file
+# TODO: make this a "Face-Vertex Mesh" -> the most widely used mesh representation
+# NOTE: 'Face': usually consists of triangles
+using Meshes, GeometryBasics
+
+faces = decompose(QuadFace{GLIndex}, Tesselation(Rect(0, 0, 1, 1), size(u_predict)))   # ? faces of the shape of a Quadrat
+# faces = decompose(UV(GeometryBasics.Vec3f), Tesselation(Rect(0, 0, 1, 1), size(u_predict)))
+gl_points = GLMakie.Point.(xs,ys,u_predict) |> vec
+
+gb_mesh = GeometryBasics.Mesh(gl_points, faces)  # create the mesh
+FileIO.save("assets/poisson.stl", gb_mesh)
+
+f, ax, pl = Makie.mesh(gb_mesh,  color = rand(100, 100), colormap=:blues)  # plot the mesh
+
+
+# GeometryBasics.Mesh(Tesselation(surface,64))
+# gb_rect = GeometryBasics.Rect3f(GeometryBasics.Vec3f(xs),GeometryBasics.Vec3f(xs))
+
+
+# GeometryBasics.connect.([tuple(xs'),tuple(ys')], Quadrangle)
+
+
+
+
+
+
+
+#######
+using GeometryBasics, LinearAlgebra, GLMakie, FileIO
+
+# Create vertices for a Sphere
+r = 0.5f0
+n = 30
+θ = LinRange(0, pi, n)
+φ2 = LinRange(0, 2pi, 2 * n)
+x2 = [r * cos(φv) * sin(θv) for θv in θ, φv in φ2]
+y2 = [r * sin(φv) * sin(θv) for θv in θ, φv in φ2]
+z2 = [r * cos(θv) for θv in θ, φv in 2φ2]
+points = vec([Point3f(xv, yv, zv) for (xv, yv, zv) in zip(x2, y2, z2)])
+
+# The coordinates form a matrix, so to connect neighboring vertices with a face
+# we can just use the faces of a rectangle with the same dimension as the matrix:
+faces = decompose(QuadFace{GLIndex}, Tesselation(Rect(0, 0, 1, 1), size(z2)))
+# Normals of a centered sphere are easy, they're just the vertices normalized.
+normals = normalize.(points)
+
+# Now we generate UV coordinates, which map the image (texture) to the vertices.
+# (0, 0) means lower left edge of the image, while (1, 1) means upper right corner.
+function gen_uv(shift)
+    return vec(map(CartesianIndices(size(z2))) do ci
+        tup = ((ci[1], ci[2]) .- 1) ./ ((size(z2) .* shift) .- 1)
+        return Vec2f(reverse(tup))
+    end)
+end
+
+# We add some shift to demonstrate how UVs work:
+uv = gen_uv(0.0)
+# We can use a Buffer to update single elements in an array directly on the GPU
+# with GLMakie. They work just like normal arrays, but forward any updates written to them directly to the GPU
+uv_buff = Buffer(uv)
+gb_mesh = GeometryBasics.Mesh(meta(points; uv=uv_buff, normals), faces)
+
+f, ax, pl = mesh(gb_mesh,  color = rand(100, 100), colormap=:blues)
+wireframe!(ax, gb_mesh, color=(:black, 0.2), linewidth=2, transparency=true)
+record(f, "uv_mesh.mp4", LinRange(0, 1, 100)) do shift
+    uv_buff[1:end] = gen_uv(shift)
+end
